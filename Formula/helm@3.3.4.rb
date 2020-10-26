7e942b0e2f49508b051c58c72d0648483437ab5c
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2020-10-26 12:26:27 -0400

    add helm formulae

---
+++ Formula/helm@3.3.4.rb
@@ -0,0 +1,38 @@
+class HelmAT334 < Formula
+  desc "Kubernetes package manager"
+  homepage "https://helm.sh/"
+  url "https://github.com/helm/helm.git",
+      tag:      "v3.3.4",
+      revision: "a61ce5633af99708171414353ed49547cf05013d"
+  license "Apache-2.0"
+  head "https://github.com/helm/helm.git"
+
+  depends_on "go" => :build
+
+  def install
+    system "make", "build"
+    bin.install "bin/helm"
+
+    mkdir "man1" do
+      system bin/"helm", "docs", "--type", "man"
+      man1.install Dir["*"]
+    end
+
+    output = Utils.safe_popen_read({ "SHELL" => "bash" }, bin/"helm", "completion", "bash")
+    (bash_completion/"helm").write output
+
+    output = Utils.safe_popen_read({ "SHELL" => "zsh" }, bin/"helm", "completion", "zsh")
+    (zsh_completion/"_helm").write output
+  end
+
+  test do
+    system "#{bin}/helm", "create", "foo"
+    assert File.directory? "#{testpath}/foo/charts"
+
+    version_output = shell_output("#{bin}/helm version 2>&1")
+    assert_match "Version:\"v#{version}\"", version_output
+    if build.stable?
+      assert_match stable.instance_variable_get(:@resource).instance_variable_get(:@specs)[:revision], version_output
+    end
+  end
+end
--- README.md
+++ README.md
@@ -1,9 +1,25 @@
-# Codecademy-engineering Bootstrap
+# Codecademy Engineering Homebrew Bootstrap
+
+> Homebrew’s versions should not be used to “pin” formulae to your personal requirements. You should instead create your own tap for formulae you or your organisation wish to control the versioning of
+- https://docs.brew.sh/Versions
+
+This is Codecademy's "tap", allowing control over the specific versions of formulae we need.
 
 ## How do I install these formulae?
+
 `brew install codecademy-engineering/bootstrap/<formula>`
 
 Or `brew tap codecademy-engineering/bootstrap` and then `brew install <formula>`.
 
+## Adding new Formulae
+
+To add a formula version that is otherwise available in Homebrew core, use the `extract` command, e.g.:
+```
+brew extract --version 3.3.4 helm codecademy-engineering/bootstrap
+```
+
+Or you may manually write formulae in `./Formula/`.
+
 ## Documentation
+
 `brew help`, `man brew` or check [Homebrew's documentation](https://docs.brew.sh).
