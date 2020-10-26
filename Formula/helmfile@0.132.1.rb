00a51fb9bff3cab4346196006e9165594d7765f4
Brian H. Buchholz <4773480+bhb603@users.noreply.github.com>
2020-10-26 13:42:40 -0400

    add helmfile

---
+++ Formula/helmfile@0.132.1.rb
@@ -0,0 +1,36 @@
+class HelmfileAT01321 < Formula
+  desc "Deploy Kubernetes Helm Charts"
+  homepage "https://github.com/roboll/helmfile"
+  url "https://github.com/roboll/helmfile/archive/v0.132.1.tar.gz"
+  sha256 "6b1ca959a64caabd840df0ae0e1c59bb2a4f245c7f42feb53a0a50cc84da2ace"
+  license "MIT"
+
+  depends_on "go" => :build
+  # depends_on "helm"
+
+  def install
+    system "go", "build", "-ldflags", "-X github.com/roboll/helmfile/pkg/app/version.Version=v#{version}",
+             "-o", bin/"helmfile", "-v", "github.com/roboll/helmfile"
+  end
+
+  test do
+    (testpath/"helmfile.yaml").write <<-EOS
+    repositories:
+    - name: stable
+      url: https://kubernetes-charts.storage.googleapis.com
+
+    releases:
+    - name: vault                            # name of this release
+      namespace: vault                       # target namespace
+      createNamespace: true                  # helm 3.2+ automatically create release namespace (default true)
+      labels:                                # Arbitrary key value pairs for filtering releases
+        foo: bar
+      chart: stable/vault                    # the chart being installed to create this release, referenced by `repository/chart` syntax
+      version: ~1.24.1                       # the semver of the chart. range constraint is supported
+    EOS
+    system Formula["helm"].opt_bin/"helm", "create", "foo"
+    output = "Adding repo stable https://kubernetes-charts.storage.googleapis.com"
+    assert_match output, shell_output("#{bin}/helmfile -f helmfile.yaml repos 2>&1")
+    assert_match version.to_s, shell_output("#{bin}/helmfile -v")
+  end
+end
